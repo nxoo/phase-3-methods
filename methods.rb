45bef4f5f639b539170575ed4a5e82c46e04d4c3
nxoo <nxoo.dev@gmail.com>
2022-10-24 07:37:00 +0300

    test passed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(param)
+  puts "Hello, " + param + '!'
+end
+
+def greet_with_default(param="programmer")
+  puts "Hello, " + param + '!'
+end
+
+def add(x, y)
+  x + y
+end
+
+def halve(x)
+  if x.class != Integer
+    return nil
+  end
+  x / 2
+end
